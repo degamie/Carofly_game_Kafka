642e2046e9f0657fc0a5cd5c19dc730b1a268b6f
Degamie_sign <degamiesign@gmail.com>
2026-06-18 18:12:52 +0530

    Param_Constructor#1

--- src/main/java/com/kafka/Carofly/dto/PlayerProducer.java
+++ src/main/java/com/kafka/Carofly/dto/PlayerProducer.java
@@ -1,4 +1,4 @@
-//WID(29/05/2026)(Sarthak Mittal(DegamieSign)#1.1.1
+//WID(18/05/2026)(Sarthak Mittal(DegamieSign)#1.1.1.1
 package com.kafka.Carofly.dto;
 
 //import com.networknt.schema.format.TimeFormat;
@@ -7,39 +7,20 @@ import lombok.*;
 
 import java.time.LocalTime;
 
-@Data
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name="PLAYER")
+
 public class PlayerProducer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="playerid",unique = true,nullable = false)
+    PlayerProducer(String playerId,String playername,int score,LocalTime playermodifieddate,Integer plaayerspeedaccuracy){
+        this.playerId=playerId;
+        this.plaayerspeedaccuracy=plaayerspeedaccuracy;
+        this.playername=playername;
+        this.score=score;
+        this.playermodifieddate=playermodifieddate;
+
+    }
     public String playerId;
-    @Column(name="playername",unique = false,nullable=true)
     public String playername;
-//    @Column(name="flighttime",unique = false,nullable=true)
-//    public TimeFormat playerFlightTime;
-    @Column(name="score",unique = false, nullable=true)
     public  int score;
-    @Column(name="logoutdate",unique = false,nullable=true)
     public LocalTime playermodifieddate;
-    @Column(name="plaayerspeedaccuracy",unique = false,nullable=true)
     public  Integer plaayerspeedaccuracy;
-
 }
-
-
-////    @Column(name="logintime",unique = false)
-//    public LocalTime  loginTime;
-////    @Column(name="logoutTime",unique = false)
-//    public LocalTime  logoutTime;
-////    @Column(name="loginate",unique = false)
-//    public SimpleDateFormat playercrdate;
-
-////    @Column(name="playeremail",unique = false)
-//    public LocalTime  email;
 
