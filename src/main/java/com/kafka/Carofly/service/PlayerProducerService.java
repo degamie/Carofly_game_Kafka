ec5c4c872bfcf35ad06b7cbba0689a447707f026
Degamie_sign <degamiesign@gmail.com>
2026-06-19 13:04:40 +0530

    Player dto's Autowiring#1

--- src/main/java/com/kafka/Carofly/service/PlayerProducerService.java
+++ src/main/java/com/kafka/Carofly/service/PlayerProducerService.java
@@ -1,4 +1,10 @@
+//WID(19/6/2026)(Sarthak Mittal(DegamieSign)
 package com.kafka.Carofly.service;
 
+import com.kafka.Carofly.dto.PlayerProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PlayerProducerService {
+    @Autowired
+    public PlayerProducer producer;
 }
