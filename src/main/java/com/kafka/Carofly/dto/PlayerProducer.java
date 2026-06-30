6141131697a1e4d98544f4bf63ad1d050e907fd8
Degamie_sign <degamiesign@gmail.com>
2026-06-30 18:29:21 +0530

    PlayerId_binding#1

--- src/main/java/com/kafka/Carofly/dto/PlayerProducer.java
+++ src/main/java/com/kafka/Carofly/dto/PlayerProducer.java
@@ -1,4 +1,4 @@
-//WID(27/05/2026)(Sarthak Mittal(DegamieSign)(ProducerCls)
+//WID(30/05/2026)(Sarthak Mittal(DegamieSign)(ProducerCls)
 package com.kafka.Carofly.dto;
 
 //import com.networknt.schema.format.TimeFormat;
@@ -8,6 +8,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 
 
 public class PlayerProducer {
+    public void setPlayerId(String playerId){this.playerId=playerId;}//binding PlayerId in GameApp
     public void setplayerSpeedaccuracy(Integer playerspeedaccuracy){this.playerspeedaccuracy=playerspeedaccuracy;}
     public KafkaTemplate<String,String> kafkaTemplate=null;
     public String getPlayerId(String playerId){return playerId;}//Fetching PlayerID in GameApp
