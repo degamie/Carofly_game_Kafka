c4ac83dc6ccac339ee00e344bf8a1b0bee9dd0cf
Degamie_sign <degamiesign@gmail.com>
2026-07-26 09:15:29 +0530

    Param_Constr_asyncConfig

--- src/main/java/com/kafka/Carofly/Config/AsyncConfigLoader.java
+++ src/main/java/com/kafka/Carofly/Config/AsyncConfigLoader.java
@@ -1,7 +1,10 @@
+//WID(26/7/2026)(Sarthak Mittal)
 package com.kafka.Carofly.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+@Configuration
 
 @EnableAsync
 public class AsyncConfigLoader {
@@ -11,5 +14,8 @@ public class AsyncConfigLoader {
         executor.setMaxPoolSize(250);
         executor.setThreadNamePrefix("-player_producer");
         return executor;
+    }
+    public AsyncConfigLoader(ThreadPoolTaskExecutor executor){
+        this.executor=executor;
     }
 }
