be706b02feb6f6f7c115cd613327549a145dfda9
Degamie_sign <degamiesign@gmail.com>
2026-08-01 14:49:56 +0530

    cacheManager

--- src/main/java/com/kafka/Carofly/Config/CacheCaffieneLoader.java
+++ src/main/java/com/kafka/Carofly/Config/CacheCaffieneLoader.java
@@ -1,13 +1,19 @@
+//WID(1/8/2026)(Sarthak Mittal(DegameiSign)
 package com.kafka.Carofly.Config;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collection;
+import java.util.List;
+
 @Configuration
 @EnableCaching
-public class CacheCaffieneLoader {
+public class CacheCaffieneLoader implements CacheManager{
     CacheCaffieneLoader cacheCaffieneLoader;
     public  CacheCaffieneLoader(CacheCaffieneLoader cacheCaffieneLoader){
         this.cacheCaffieneLoader=cacheCaffieneLoader;
@@ -15,6 +21,23 @@ public class CacheCaffieneLoader {
     @Bean
     public CacheManager cacheManager(){
         CaffeineCacheManager cacheManager=new CacheManager();
-        cacheManager.setCacheNames("players","gameStatus","Options");
+        cacheManager.setCacheNames("players","gameStatus","Options")(
+                .maximumSize(10)
+                .build());
+    }
+
+    @Override
+    public @Nullable Cache getCache(String name) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getCacheNames() {
+        return List.of();
+    }
+
+    @Override
+    public void resetCaches() {
+        CacheManager.super.resetCaches();
     }
 }
