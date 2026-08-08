4acb7ca7e4c9c7a7636384a5eb0d70167cec810a
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-08 13:32:33 +0530

    Add files via upload

---
+++ ChatClientService.java
@@ -0,0 +1,25 @@
+//WID(8/8/2026)(Sarthak Mittal(DegamieSign))#Impl
+package com.kafka.Carofly.service;
+
+import com.kafka.Carofly.dto.ChatMessage;
+import com.kafka.Carofly.dto.PlayerConsumerdto;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class ChatClientService {
+    void setTaskExecutor(ThreadPoolTaskExecutor taskExecutor){
+        this.taskExecutor=taskExecutor;
+    }
+    ThreadPoolTaskExecutor taskExecutor=new ThreadPoolTaskExecutor();
+@Async("aiTaskExecutor-player")
+    public CompletableFuture<PlayerConsumerdto>generateaiasyncResponse(ChatMessage chatMessage) {
+    String reply="AI Response(Async)"+chatMessage.getMessage();
+}
+}
+
+//    taskExecutor.setMaxPoolSize(150);
+//    taskExecutor.initialize();
