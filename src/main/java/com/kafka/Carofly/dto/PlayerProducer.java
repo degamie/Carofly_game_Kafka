5480982c56c7d98c0a9a48d0c120f8246716f37f
unknown <degamiesign@gmail.com>
2026-08-17 20:06:47 +0530

    updating#player#modifieddate

--- src/main/java/com/kafka/Carofly/dto/PlayerProducer.java
+++ src/main/java/com/kafka/Carofly/dto/PlayerProducer.java
@@ -1,4 +1,4 @@
-//WID(23/07/2026)(Sarthak Mittal(DegamieSign)(ProducerCls)#1c
+//WID(17/08/2026)(Sarthak Mittal(DegamieSign)(ProducerCls)#1c
 package com.kafka.Carofly.dto;
 
 //import com.networknt.schema.format.TimeFormat;
@@ -10,6 +10,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 
 
 public class PlayerProducer {
+    public void updateBYdatemodifiedcreated(DateTimeFormatters playermodifieddate){
+        getmodifiedDate(playermodifieddate)+setPlayermodifieddate(playermodifieddate)+1;
+    }
     public void updateByplayerSpeedAccruacy(Integer playerSpeedAccuracy){
         getplaayerspeedaccuracy(playerSpeedAccuracy)+setplayerSpeedaccuracy(playerSpeedAccuracy)+1;
     }
