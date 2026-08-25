90871d553c9a0f14afc9cabcb28717a7d65a526b
unknown <degamiesign@gmail.com>
2026-08-25 16:46:42 +0530

    playerProducer#kafka

--- src/main/java/com/kafka/Carofly/Config/kafkaTopic.java
+++ src/main/java/com/kafka/Carofly/Config/kafkaTopic.java
@@ -1,4 +1,4 @@
-//WID(24/8/2026)(Sarthak Mittal(DegmaieSign)(Kafka Topic)
+//WID(25/8/2026)(Sarthak Mittal(DegmaieSign)(Kafka Topic)#1
 package com.kafka.Carofly.Config;
 
 import com.kafka.Carofly.dto.PlayerProducer;
@@ -6,10 +6,12 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.internals.Topic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.yaml.snakeyaml.internal.Logger;
 
 @Configuration
 public class kafkaTopic {
+    KafkaTemplate<String,PlayerProducer> producerKafkaTemplate=new KafkaTemplate<>();
     String topicName;
     @Bean
     public void updateByplayerTopic(String playername){
